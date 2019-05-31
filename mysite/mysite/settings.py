3e5506fcad15bc34a7c101774c658c871913ff72
Iván De los Santos García <ivandega22@gmail.com>
2019-05-31 17:42:28 +0200

    Changes for deployment

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -85,7 +85,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
 )
 
-BASEURL = 'http://localhost:8080'
+BASEURL = 'https://front5-datame.herokuapp.com'
 
 ROOT_URLCONF = 'mysite.urls'
 
@@ -188,7 +188,7 @@ PAYPAL_MODE= "sandbox"
 PAYPAL_CLIENT_ID = "AQnKuEyaCgxZsEBPC-aq3glrpVK1dcTUUbvjkmnguZOM1WLQnsEU-y7i2oh3VkM_Xm4AfbJQx8AKdiYF"
 PAYPAL_CLIENT_SECRET = "EEkGJcVWziLr8FnQFC-BCYX0bcSz_xyh3POT7iR1K9e7fxFLVhI9EJSnOl5Jn3d_UhqydqGEYTfzCozc"
 
-SITE_URL ='http://localhost:8080/'
+SITE_URL ='https://front5-datame.herokuapp.com/'
 
 import django_heroku
 django_heroku.settings(locals())
